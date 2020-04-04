01b368a7c1b6cbdaa8e8e993f906aa68003d8f3c
Sidi Meisanjaya <Sidi.Meisanjaya@gmail.com>
2020-04-04 16:07:50 +0900

    database and some class

---
+++ database/migrations/2020_03_30_040356_create_applicant_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApplicantTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applicants', function (Blueprint $table) {
+	    $table->id();
+	    $table->integer('user_id');
+	    $table->string('email');
+	    $table->double('monthlyIncome', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applicants');
+    }
+}
